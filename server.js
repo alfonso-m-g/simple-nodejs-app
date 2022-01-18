ca6aef258052eec96a10b7a581919512b484911c
alfonso-m-g <alfonso96xd@gmail.com>
2022-01-18 17:08:51 -0600

    Modified server.js yet again

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ mongoose.connect(mongodb)
 
 
 const app = express();
-app.get('/',(req,res)=>res.send('Hello my name is Alfonso Morales :D'));
+app.get('/',(req,res)=>res.send('Estoy enamorado de mi novia Faith Coronado Meza, alias ratón. Te amo nena <3'));
 
 const port = process.env.PORT || 8080;
 
